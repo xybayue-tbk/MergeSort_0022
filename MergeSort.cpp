025f1b54c2370c3b3c1d492166724014a4b3acdc
xybayue-tbk <xybayue@gmail.com>
2026-04-02 15:36:58 +0700

    looping + percabangan

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -49,4 +49,19 @@ void mergeSort(int low, int high){
     int i = low;
     int j = mid + 1;
     int k = low; 
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] <= arr[j])
+        {
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            B[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
 }
