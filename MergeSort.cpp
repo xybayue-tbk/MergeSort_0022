c599c2f95bfd9e060d4b195d878ee205e4911022
xybayue-tbk <xybayue@gmail.com>
2026-04-02 15:45:51 +0700

    fungsi + lopping

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -84,3 +84,13 @@ void mergeSort(int low, int high){
         arr[x] = B[x];
     }
 }
+
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
